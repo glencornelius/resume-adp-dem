c3862c55b77adea817290d43e2ee926d4cf8f08d
glencornelius <glencornelius7@gmail.com>
2026-05-18 21:21:18 +0800

    fix nextjs security deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-﻿/** @type {import('next').NextConfig} */
+﻿import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-  experimental: {
-    typedRoutes: true
-  },
+  outputFileTracingRoot: __dirname,
+  typedRoutes: true,
   images: {
     remotePatterns: [
       {
